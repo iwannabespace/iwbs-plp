b190487b715d483a216635cd70e7cff8ed6ca0bb
Mystic Chai <gerzekherif@gmail.com>
2023-05-12 20:45:21 +0300

    Update: Reading multiple lines instead of one line

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -11,11 +11,11 @@ namespace iwbs
     {
     }
 
-    Lexer::Lexer(const std::string& filename)
+    Lexer::Lexer(const std::string &filename)
         : filename(filename)
     {
         if (!std::filesystem::exists(filename))
-        {    
+        {
             this->filename.clear();
             throw "File couldn't be found!";
         }
@@ -35,11 +35,12 @@ namespace iwbs
             size_t number = 1;
 
             file.open(filename, std::ios::in);
-            
+            std::string lineToken;
+            int numberOfQuotes = 0;
+
             while (std::getline(file, lineStr))
             {
-                std::string lineToken;
-                int numberOfQuotes = 0;
+                line.setLineNumber(number++);
 
                 for (size_t i = 0; i < lineStr.length(); i++)
                 {
@@ -57,9 +58,9 @@ namespace iwbs
 
                     if (lineStr[i] == ' ' && !numberOfQuotes)
                     {
-                        if (lineToken.length() != 1) // This is checked because, if there is an empty char after a semicolon this code block would push an empty token to the line  
+                        if (lineToken.length() != 1) // This is checked because, if there is an empty char after a semicolon this code block would push an empty token to the line
                             line.pushToken(Token(lineToken.substr(0, lineToken.length() - 1)));
-                        
+
                         lineToken.clear();
                     }
 
@@ -70,12 +71,13 @@ namespace iwbs
 
                         line.pushToken(Token(";", Token::Type::Semicolon));
                         lineToken.clear();
+                        lines.push_back(line);
+                        line.clear();
                     }
                 }
 
-                line.setLineNumber(number++);
-                lines.push_back(line);
-                line.clear();
+                //     lines.push_back(line);
+                //    line.clear();
             }
 
             file.close();
@@ -83,7 +85,7 @@ namespace iwbs
         }
     }
 
-    void Lexer::setFile(const std::string& filename)
+    void Lexer::setFile(const std::string &filename)
     {
         if (!std::filesystem::exists(filename))
             throw "File couldn't be found!";
@@ -91,7 +93,7 @@ namespace iwbs
         this->filename = filename;
     }
 
-    const std::string& Lexer::getFile() const
+    const std::string &Lexer::getFile() const
     {
         return filename;
     }
@@ -107,7 +109,7 @@ namespace iwbs
         filename.clear();
     }
 
-    const Line& Lexer::operator[](size_t index) const
+    const Line &Lexer::operator[](size_t index) const
     {
         if (index < 0 || index >= lines.size())
             throw "Index out of bounds for Lexer::lines container";
@@ -115,7 +117,7 @@ namespace iwbs
         return lines[index];
     }
 
-    Line& Lexer::operator[](size_t index)
+    Line &Lexer::operator[](size_t index)
     {
         if (index < 0 || index >= lines.size())
             throw "Index out of bounds for Lexer::lines container";
@@ -125,28 +127,27 @@ namespace iwbs
 
     void Lexer::analyze()
     {
-        for (Line& line : lines)
+        for (Line &line : lines)
         {
             int32_t numberOfSemicolons = line.count(Token(";", Token::Type::Semicolon));
             size_t lineNumber = line.getLineNumber();
 
-            if (numberOfSemicolons != 1)
-            {   
-                line.setError(Error(numberOfSemicolons < 1 ? "Missing semicolon error" : "Multiple semicolons error", lineNumber));
+            if (numberOfSemicolons < 1)
+            {
+                line.setError(Error("Missing semicolon error", lineNumber));
                 continue;
             }
-            
+
             else
             {
                 if (line.contains("="))
                 {
                     if (line.getNumberOfTokens() < 5)
                     {
-                        
                     }
 
                     if (line.count("=") > 1)
-                    {    
+                    {
                         line.setError(Error("Multiple assignment operator error", lineNumber));
                         continue;
                     }
@@ -160,7 +161,7 @@ namespace iwbs
                     Variable::Type type = Variable::Resolve(line[0].getValue());
                     if (type != Variable::Type::Unknown)
                         line[0].setType(Token::Type::VariableType);
-                    
+
                     else
                     {
                         line.setError(Error("Unknown token error", lineNumber));
@@ -170,19 +171,18 @@ namespace iwbs
                     bool isValidName = Variable::IsValidVariableName(line[1].getValue());
                     if (isValidName)
                         line[1].setType(Token::Type::VariableName);
-                    
+
                     else
                     {
                         line.setError(Error("Incorrect variable naming error", lineNumber));
                         continue;
                     }
-
                 }
             }
         }
     }
 
-    std::vector<std::string> Lexer::split(const std::string& str, const std::string& delim) const
+    std::vector<std::string> Lexer::split(const std::string &str, const std::string &delim) const
     {
         std::vector<std::string> result;
         size_t start = 0;

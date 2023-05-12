9ac1e79b401e7974214c53f34eafb14f138bfb23
Mystic Chai <gerzekherif@gmail.com>
2023-05-12 20:48:00 +0300

    minor fix

--- src/variable.cpp
+++ src/variable.cpp
@@ -1,13 +1,14 @@
+#include <algorithm>
 #include "../include/variable.hpp"
 
 namespace iwbs
 {
     Variable::Variable()
         : type(Type::Unknown), name(""), value("")
-    {   
+    {
     }
 
-    Variable::Variable(Type type, const std::string& name, const std::string& value)
+    Variable::Variable(Type type, const std::string &name, const std::string &value)
         : type(type), name(name), value(value)
     {
     }
@@ -21,12 +22,12 @@ namespace iwbs
         this->type = type;
     }
 
-    void Variable::setName(const std::string& name)
+    void Variable::setName(const std::string &name)
     {
         this->name = name;
     }
 
-    void Variable::setValue(const std::string& value)
+    void Variable::setValue(const std::string &value)
     {
         this->value = value;
     }
@@ -36,49 +37,48 @@ namespace iwbs
         return type;
     }
 
-    const std::string& Variable::getName() const
+    const std::string &Variable::getName() const
     {
         return name;
     }
 
-    const std::string& Variable::getValue() const
+    const std::string &Variable::getValue() const
     {
         return value;
     }
 
-    bool Variable::operator==(const Variable& rhs) const
+    bool Variable::operator==(const Variable &rhs) const
     {
         return name == rhs.name;
     }
 
-    bool Variable::operator!=(const Variable& rhs) const
+    bool Variable::operator!=(const Variable &rhs) const
     {
         return !(*this == rhs);
     }
 
-    Variable::Type Variable::Resolve(const std::string& value)
+    Variable::Type Variable::Resolve(const std::string &value)
     {
         if (value == "integer")
             return Type::Integer;
-        
+
         if (value == "float")
             return Type::Float;
-        
+
         if (value == "string")
             return Type::String;
-        
+
         if (value == "boolean")
             return Type::Boolean;
-        
+
         return Type::Unknown;
     }
 
-    bool Variable::IsValidVariableName(const std::string& name)
+    bool Variable::IsValidVariableName(const std::string &name)
     {
         bool startsWithAlpha = std::isalpha(name[0]) || name[0] == '_';
-        bool namingValid = std::all_of(name.begin() + 1, name.end(), [](char c) {
-             return std::isalpha(c) || std::isdigit(c) || c == '_'; 
-        });
+        bool namingValid = std::all_of(name.begin() + 1, name.end(), [](char c)
+                                       { return std::isalpha(c) || std::isdigit(c) || c == '_'; });
 
         return startsWithAlpha && namingValid;
     }
